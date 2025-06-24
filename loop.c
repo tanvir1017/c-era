2f82b0ac364000aeb6d25adf8910b5cf3ffbbad3
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-06-24 23:40:50 +0600

    feat: 🎸 two types of loop in C #include <cs50.h>
    int main(void){
    
    BREAKING CHANGE: 🧨 ✨

---
+++ loop.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+
+    /*
+    int i = 1;
+    while(i <= 5){
+        printf("index is %i\n", i);
+        i++;
+    }
+        */
+
+        for(int i = 1; i <= 10; i++){
+            printf("The value of I is %i\n", i);
+        }
+}
