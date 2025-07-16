dc827aebf4cff9b62e2e5ca1c1369f6fd53e0c7e
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-07-16 07:56:32 +0600

    feat: 🎸 Array length. Custom function and prototype in C

--- array.c
+++ array.c
@@ -1,13 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
-int main () {
+int get_string_length(string s);
+int main()
+{
     string name = get_string("Name: ");
+    int string_length = get_string_length(name);
+    printf("%i\n", string_length);
+}
 
+
+// A function which calculate the total length of an given string
+int get_string_length(string s){
     int n = 0;
-    while(name[n] != '\0'){
+    while (s[n] != '\0'){
         n++;
     }
-    printf("%i\n", n);
+
+    return n;
 }
